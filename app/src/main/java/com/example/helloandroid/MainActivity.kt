490bbde9c5ac4e15987bc3d60d76c89dfe437efc
StoicaVlad <stoicavlad2000@gmail.com>
2021-05-16 22:39:26 +0300

    commit test

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
